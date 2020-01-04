ff4ead3326d7f8f769f01ae90e3a9d7fb2a394a4
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-04 19:06:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,7 +9,13 @@ end
 
 def find_max_value(array)
   max = array[0]
-  
+  array.length.times do |count|
+    if array[count] > max
+      max = array[count]
+    end
+  end
+  max
+end
 
 def find_min_value(array)
   min = 0
