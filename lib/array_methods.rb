424f74e6151e62d9bb2baa394cf119efce630735
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-04 18:51:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,9 @@ def find_element_index(array, value_to_find)
    array.length.times do |count|
     if array[count] == value_to_find
     return count
-  else nil
    end
   end
+  nil
 end  
 
 def find_max_value(array)
