380d0e49add53f04e496d00bf5973e4ca642f078
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-04 18:44:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,8 @@ def find_element_index(array, value_to_find)
    array.length.times do |count|
     if array[count] == value_to_find
     return count
-  else
-    return nil
    end
+   nil
   end
 end  
 
