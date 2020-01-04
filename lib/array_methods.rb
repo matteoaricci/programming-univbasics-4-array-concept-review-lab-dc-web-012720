d76598d610faa40da1c194d10f69451c7b893dbc
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-04 18:51:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,11 +8,9 @@ def find_element_index(array, value_to_find)
 end  
 
 def find_max_value(array)
-  array.sort!()
-  array[-1]
+  
 end
 
 def find_min_value(array)
-  array.sort!()
-  array[0]
+  
 end
